87aa0a28b6f921601aae352a6ca45b95eed84169
Nickolas Nielsen <nielsenn@purdue.edu>
2025-03-01 16:30:50 -0500

    not working

--- daq_system/processing/analog.py
+++ daq_system/processing/analog.py
@@ -31,6 +31,8 @@ def process_analog_input(
 
         # Create AI voltage channel
         ai_chan = ni.AIVoltageChan(
+            min_val=0.0,
+            max_val=10.0,
             channel=sensor_channel.key,
             port=channel_num,
             device=device.key,
