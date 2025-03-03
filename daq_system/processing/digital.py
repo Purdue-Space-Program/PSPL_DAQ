4dc577f5a2fc3f8a610bfaa91a94ea2c16827796
Nickolas Nielsen <nielsenn@purdue.edu>
2025-03-03 03:40:16 -0500

    testing commit

--- daq_system/processing/digital.py
+++ daq_system/processing/digital.py
@@ -3,6 +3,8 @@ import synnax as sy
 from synnax.hardware import ni
 from .channel_factory import ChannelFactory
 
+STATE_RATE = 100
+
 
 def process_digital_input(
     data: pd.ExcelFile,
@@ -12,25 +14,32 @@ def process_digital_input(
     stream_rate: int,
 ):
     """Process digital input configuration"""
+
     sensors = data.parse("DI")
 
     for _, row in sensors.iterrows():
+
         # Create timestamp channel
+
         name = row["Name"]
+
         bcls_di_time = channel_factory.create_timestamp_channel(f"BCLS_di_time_{name}")
 
         # Create sensor channel - note: no units for digital channels
+
         sensor_channel = channel_factory.create_data_channel(
             name=row["Name"],
             data_type=sy.DataType.UINT8,
             index_key=bcls_di_time.key,
-            rate=sy.Rate.HZ * stream_rate,
+            rate=sy.Rate.HZ * STATE_RATE,
         )
 
         # Extract channel number
+
         channel_num = int("".join(filter(str.isdigit, row["Channel"].split("/")[-1])))
 
         # Create DI channel
+
         di_chan = ni.DIChan(
             channel=sensor_channel.key,
             port=0,
@@ -48,37 +57,45 @@ def process_digital_output(
     sample_rate: int,
 ):
     """Process digital output configuration"""
+
     sensors = data.parse("DO")
 
     for _, row in sensors.iterrows():
+
         # Create timestamp channels
+
         name = row["Name"]
+
         bcls_state_time = channel_factory.create_timestamp_channel(
             f"BCLS_state_time_{name}"
         )
+
         bcls_cmd_time = channel_factory.create_timestamp_channel(
             f"BCLS_cmd_time_{name}"
         )
 
         # Extract line number
+
         line = int(row["Channel"].split("/")[-1][4:])
 
         # Create state and command channels - note: no units for digital channels
+
         state_chan = channel_factory.create_data_channel(
             name=f"{row['Name']}_state",
             data_type=sy.DataType.UINT8,
             index_key=bcls_state_time.key,
-            rate=sy.Rate.HZ * sample_rate,
+            rate=sy.Rate.HZ * STATE_RATE,
         )
 
         cmd_chan = channel_factory.create_data_channel(
             name=f"{row['Name']}_cmd",
             data_type=sy.DataType.UINT8,
             index_key=bcls_cmd_time.key,
-            rate=sy.Rate.HZ * sample_rate,
+            rate=sy.Rate.HZ * STATE_RATE,
         )
 
         # Create DO channel
+
         do_chan = ni.DOChan(
             cmd_channel=cmd_chan.key,
             state_channel=state_chan.key,
